423e415bcc8740d287938fe0ccc089fa4e35bda4
Ilia Shogin <ilia.shogin2@gmail.com>
2026-03-25 15:16:15 +0200

    init added

---
+++ init.sql
@@ -0,0 +1,11 @@
+INSERT INTO categories (name) VALUES
+    ("Sauna"),
+    ("Laundry"),
+    ("Drying room"),
+    ("Club room");
+
+INSERT INTO spaces (name, default_category_id) VALUES
+    ("Sauna", 1),
+    ("Laundry room", 2),
+    ("Drying room", 3),
+    ("Club room", 4);
